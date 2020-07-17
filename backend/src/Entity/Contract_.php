e03ea28e2d527acf99f0be73e1b468508dfddad5
Sara bouglam <sarabouglam@gmail.com>
2020-07-17 17:39:20 +0100

    undoing the db modification

--- backend/src/Entity/Contract_.php
+++ backend/src/Entity/Contract_.php
@@ -1,67 +1,113 @@
 <?php
-    namespace App\Entity; 
-    use Doctrine\Common\Collections\Collection;
-    use DateTime;
-    use Doctrine\ORM\Mapping as ORM; 
+    namespace App\Entity;
+
+        use DateTime;
+        use Doctrine\Common\Collections\Collection;
+        use Doctrine\ORM\Mapping as ORM;
+
         /**
          * @ORM\Entity(repositoryClass="App\Repository\Contract_Repository")
-         * 
+         *
          */
-        class Contract_ { 
+        class Contract_
+        {
             /**
              * @ORM\Id
              * @ORM\GeneratedValue
              * @ORM\Column(type="integer")
              */
             private $id;
-                /** @ORM\Column(type="integer" , length=200)*/
+            /** @ORM\Column(type="integer" , length=200)*/
 
-            private $date_;  // this date concerns the contract 
-                /**
-                 * @ORM\ManyToOne(targetEntity="App\Entity\Client")
-                 * @ORM\JoinColumn(name="client" , referencedColumnName="id")
-                */
-            private $client; 
-                /**
-                 * @ORM\OneToMany(targetEntity="App\Entity\Invoice" , mappedBy="contract")
-                 */
-            private $invoices; 
-                /**
-                 * @ORM\ManyToOne(targetEntity="App\Entity\Agency")
-                 * @ORM\JoinColumn(name="arrival_agency" , referencedColumnName="id")
-                 */
+            private $date_; // this date concerns the contract
 
-            function __construct( )
+            /** @ORM\Column(type="time" )*/
+            private $arrival;
+            /** @ORM\Column(type="time" )*/
+            private $departure;
+
+            /**
+             * @ORM\ManyToOne(targetEntity="App\Entity\Client")
+             * @ORM\JoinColumn(name="client" , referencedColumnName="id" , nullable=false)
+             */
+            private $client;
+            /**
+             * @ORM\OneToMany(targetEntity="App\Entity\Invoice" , mappedBy="contract")
+             */
+
+            private $invoices;
+            /**
+             * @ORM\ManyToOne(targetEntity="App\Entity\Vehicle")
+             * @ORM\JoinColumn(name="vehicle"  , referencedColumnName="id" , nullable=false)
+             */
+
+            private $vehicle;
+
+            public function __construct()
             {
             }
-            public function getid():int{ 
-                return $this ->id; 
+            public function getid(): int
+            {
+                return $this->id;
             }
-            public function setid(int $id){ 
-            $this->id = $id; 
-            }
-            
-            public function getDate():DateTime{ 
-                return $this ->date_; 
-            }
-            public function setDate(DateTime $date_){ 
-            $this->date_= $date_; 
-            }         
-
-            function getclient() :Client{ 
-                return $this->client; 
-            }
-            function setclient(Client $client) :void { 
-                    $this->client = $client;
-            }
- 
-
-            function getinvoices() :Collection { 
-                return $this->invoices; 
-            }
-            function setinvoices(Collection $invoices) :void { 
-                    $this->invoices = $invoices;
+            public function setid(int $id)
+            {
+                $this->id = $id;
             }
 
- 
-    }
+            public function getDate(): DateTime
+            {
+                return $this->date_;
+            }
+            public function setDate(DateTime $date_)
+            {
+                $this->date_ = $date_;
+            }
+
+            public function getclient(): Client
+            {
+                return $this->client;
+            }
+            public function setclient(Client $client): void
+            {
+                $this->client = $client;
+            }
+
+
+            public function getVehicle(): Vehicle
+            {
+                return $this->vehicle;
+            }
+            public function setVehicle(Vehicle $vehicle)
+            {
+                $this->vehicle= $vehicle;
+            }
+
+            public function getDeparture(): DateTime
+            {
+                return $this->departure;
+            }
+            public function setDeparture(DateTime $departure)
+            {
+                $this->departure = $departure;
+            }
+    
+            public function getArrival(): DateTime
+            {
+                return $this->arrival;
+            }
+            public function setArrival(DateTime $arrival)
+            {
+                $this->arrival = $arrival;
+            }     
+
+            public function getinvoices(): Collection
+            {
+                return $this->invoices;
+            }
+            public function setinvoices(Collection $invoices): void
+            {
+                $this->invoices = $invoices;
+            }
+
+        }
--- backend/src/controllers/admin/AdminContractController.php
+++ backend/src/controllers/admin/AdminContractController.php
@@ -1,43 +1,41 @@
 <?php
-namespace App\controllers\admin;
-use App\Entity\Contract_;
-use Hateoas\HateoasBuilder;
-use Hateoas\UrlGenerator\CallableUrlGenerator;
-use JMS\Serializer\SerializerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use  Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\RouterInterface;
-
-
-
-class AdminContractController extends  AbstractController { 
-    
-    private $hateoas; 
-    public function __construct( RouterInterface $router)
-    {
-      
-        $this->hateoas= HateoasBuilder::create()
-             ->setUrlGenerator(
-                null , 
-                new CallableUrlGenerator(
-                function($route , $parameter , $absolute) use ($router)  { 
-                 return $router->generate($route ,  $parameter, RouterInterface::ABSOLUTE_URL); 
-               })
-             ) ->build(); 
-        
-    }
-  /*   public function jsonToContractObject( $body) :Contract_ { 
-          $contract = new Contract_(); 
+    namespace App\controllers\admin;
  
-     }
-    /** @Route( "/admin/contract"  , name="post_contract" , methods ={"POST"} ) */
-   /* public function postContract( Request $request ) : Response{ 
-         $body= json_decode( $request->getContent(), true ) ; 
+    use App\Entity\Contract_;
+    use Hateoas\HateoasBuilder;
+    use Hateoas\UrlGenerator\CallableUrlGenerator;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\RouterInterface;
 
+    class AdminContractController extends AbstractController
+    {
 
-    }*/
+        private $hateoas;
+        public function __construct(RouterInterface $router)
+        {
 
-}
+            $this->hateoas = HateoasBuilder::create()
+                ->setUrlGenerator(
+                    null,
+                    new CallableUrlGenerator(
+                        function ($route, $parameter, $absolute) use ($router) {
+                            return $router->generate($route, $parameter, RouterInterface::ABSOLUTE_URL);
+                        })
+                )->build();
 
-?> 
+        }
+        public function jsonToContractObject($body) 
+        {
+            $contract = new Contract_();
+
+        }
+        /** @Route( "/admin/contract"  , name="post_contract" , methods ={"POST"} ) */
+        public function postContract(Request $request) 
+        {
+            $body = json_decode($request->getContent(), true);
+
+        }
+
+    }
